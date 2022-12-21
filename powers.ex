ce51b00dd042f95f272535043fa77a4b3a2b721d
Gui Heurich <guilherme.heurich@protonmail.com>
2022-12-21 12:11:31 +0000

    Estimate nth root

--- powers.ex
+++ powers.ex
@@ -9,10 +9,26 @@ defmodule Powers do
     end
   end
 
-  def raise(x, n, acc) do
+  defp raise(x, n, acc) do
     cond do
       n == 0 -> acc
       n > 0 -> raise(x, n - 1, x * acc)
+    end
+  end
+
+  def nth_root(x, n) do
+    nth_root(x, n, x / 2.0)
+  end
+
+  defp nth_root(x, n, estimate) do
+    IO.puts("Current guess is #{estimate}")
+    f = raise(estimate, n) - x
+    f_prime = n * raise(estimate, n - 1)
+    next = estimate - f / f_prime
+    change = abs(next - estimate)
+    cond do
+      change < 1.0e-8 -> next
+      true -> nth_root(x, n, next)
     end
   end
 end
