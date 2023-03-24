b75a63b4b7aa58d6268b3c1f1d04ec7b7ced7323
Gui Heurich <guliherme.heurich@protonmail.com>
2023-03-24 14:11:09 +0000

    KV GenServer exercise from Elixir guides

---
+++ kv/lib/kv.ex
@@ -0,0 +1,18 @@
+defmodule KV do
+  @moduledoc """
+  Documentation for `KV`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KV.hello()
+      :oops
+
+  """
+  def hello do
+    :oops
+  end
+end
